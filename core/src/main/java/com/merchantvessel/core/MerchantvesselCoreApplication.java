00fd1a2e112eb9aae4a9efcfd22ca12528e8691b
Daniel <Daniel@10.112.61.205>
2020-11-03 17:58:24 +0800

    Initial Commit.

---
+++ core/src/main/java/com/merchantvessel/core/MerchantvesselCoreApplication.java
@@ -0,0 +1,13 @@
+package com.merchantvessel.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantvesselCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantvesselCoreApplication.class, args);
+	}
+
+}
---
+++ core/src/test/java/com/merchantvessel/core/MerchantvesselCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merchantvessel.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantvesselCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
